46acb7b239234159b4656c053f71562e312d14bc
Anthony <anthony.cersosimo@gmail.com>
2019-07-01 18:47:07 -0400

    created instance of express, set up the default layout for handlebars, set up lisening on the server at port 3000

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 // Require in dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("Server is listening at: http://localhost:" + PORT);
+})
