40c0f77fdabc77de9b6e251999c34e0d8beb8c81
Anthony <anthony.cersosimo@gmail.com>
2019-06-30 23:04:07 -0400

    Required in mySQL node package, set up and logged the connection to the console, exported the connection to be used elsewhere

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Require in the MySQL NPM package
+var mysql = require("mysql");
+
+// Set up connection requirements for the database 'burgers_db'
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "curb3436ale7770dog!",
+  database: "burgers_db"
+});
+
+// Initiate connection to database and console log threadId
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  };
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection
+module.exports = connection;
