4bc23f615ced62fb190ecedd2ae575f53a494387
Anthony <anthony.cersosimo@gmail.com>
2019-07-01 22:59:00 -0400

    added the variable routes to give access to the different http and api routes necessary for the server to listen on, server did not fetch html upon get request at "/"

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(bodyParser.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
 app.listen(PORT, function() {
     console.log("Server is listening at: http://localhost:" + PORT);
 });
